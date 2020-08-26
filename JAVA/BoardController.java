341857f873632b7ea61f44f649536e64af5dd71e
jvvv94 <67827336+jvvv94@users.noreply.github.com>
2020-08-26 20:18:23 +0900

    Update BoardController.java

--- JAVA/BoardController.java
+++ JAVA/BoardController.java
@@ -1,7 +1,7 @@
 package controller;
 
 import java.lang.ProcessBuilder.Redirect;
-import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,13 +22,6 @@ public class BoardController {
 
 	@Autowired
 	BoardListService service;
-	
-	//MAIN_JSON
-	@RequestMapping("/jsonlist.do")
-	@ResponseBody //ajax서버에서 사용할 서버
-	public List<BoardListVO> jsonlist() {
-		return service.getBoardNameList();
-	}
 
 	// 리스트 보여주기
 	@RequestMapping("/boardlist/list.do")
@@ -40,21 +33,26 @@ public class BoardController {
 		return mav;
 	}
 
+	@RequestMapping(value = "/boardlist/add.do", method = RequestMethod.GET)
+	public String addPost() {
+		return "boardlist/board_write";
+	}
+
 	// 게시글 추가
 	@RequestMapping(value = "/boardlist/add.do", method = RequestMethod.POST)
 	public String addBoardList(BoardListVO vo, HttpServletRequest req) throws Exception {
 
 		service.addPost(vo);
 
-		return "redirect:/boardlist/boardlist.do";
+		return "redirect:/boardlist/list.do";
 	}
 
 	// 게시물 하나를 디테일하게 보기
 	@RequestMapping("/boardlist/view.do")
-	public ModelAndView view(@RequestParam("title") String title) {
+	public ModelAndView view(@RequestParam("post_id") int post_id) {
 
 		ModelAndView mav = new ModelAndView();
-		mav.addObject("boardlists", service.getPost(title));
+		mav.addObject("boardlists", service.getPost(post_id));
 		mav.setViewName("/boardlist/board_view");
 
 		return mav;
@@ -62,17 +60,17 @@ public class BoardController {
 
 	// 게시물 삭제하기
 	@RequestMapping("/boardlist/remove.do")
-	public String remove(int post_id) {
-		service.removeBoardList(post_id);
+	public String remove(String title) {
+		service.removeBoardList(title);
 
-		return "redirect:/boardlist/boardlist.do";
+		return "redirect:/boardlist/list.do";
 	}
 
 	// 게시물 수정하기
 	@RequestMapping("/boardlist/modify.do")
-	public ModelAndView modify(@RequestParam("title") String title) {
+	public ModelAndView modify(@RequestParam("post_id") int post_id) {
 		ModelAndView mav = new ModelAndView();
-		mav.addObject("boardlists", service.getPost(title));
+		mav.addObject("boardlists", service.getPost(post_id));
 		mav.setViewName("boardlist/board_modify");
 
 		return mav;
@@ -80,17 +78,21 @@ public class BoardController {
 
 	// 게시물 업데이트
 	@RequestMapping("/boardlist/update.do")
-	public String update(@ModelAttribute("boardlist") BoardListVO vo) {
+	public ModelAndView update(@ModelAttribute("boardlist") BoardListVO vo) {
+		ModelAndView mav = new ModelAndView();
 		service.updateBoardList(vo);
+		System.out.println(vo);
+		mav.addObject("boardlists", vo);
+		mav.setViewName("boardlist/board_view");
 
-		return "boardlist/board_view";
+		return mav;
 	}
 
 	// error 처리
-//	@ExceptionHandler(Exception.class)
-//	public String ex(Exception e, Model model) {
-//		model.addAttribute("Exception", e);
-//		return "error";
-//	}
+//   @ExceptionHandler(Exception.class)
+//   public String ex(Exception e, Model model) {
+//      model.addAttribute("Exception", e);
+//      return "error";
+//   }
 
 }
