aa70afc089bb85f0bf0e1eb661d5c063aa0bb43e
LimHyungang <47623969+LimHyungang@users.noreply.github.com>
2020-08-26 16:29:28 +0900

    Update UserService.java

--- JAVA/UserService.java
+++ JAVA/UserService.java
@@ -5,19 +5,19 @@ import java.util.List;
 import biz.user.vo.UserVO;
 
 public interface UserService {
-	   UserVO login(String id,String pw);
+	   UserVO login(String user_id,String pw);
 	   
 	   int addUser(UserVO user) throws Exception;
 	
-	   int IdChk(String userid) throws Exception;   
+	   int IdChk(String user_id) throws Exception;   
 	
-	   UserVO getUser(String userid);
+	   UserVO getUser(String user_id);
 	   
 	   List<UserVO> getUserList();
 	   
 	   int updateUser(UserVO user);
 	   
-	   int removeUser(String userid);
+	   int removeUser(String user_id);
 	   
 	   List<UserVO> searchUser(String condition,String keyword);
 }
