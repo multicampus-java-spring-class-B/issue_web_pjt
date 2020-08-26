ab8840d687ecbb2b40779bc108bed175ee2e62c6
jvvv94 <67827336+jvvv94@users.noreply.github.com>
2020-08-26 20:19:40 +0900

    Update BoardListService.java

--- JAVA/BoardListService.java
+++ JAVA/BoardListService.java
@@ -5,19 +5,17 @@ import java.util.List;
 import boardlist.vo.BoardListVO;
 
 public interface BoardListService {
-	
-	int addPost(BoardListVO post_num) throws Exception;
-	
-	BoardListVO getPost(String title);
-	
-	List<BoardListVO> getPostList();
-	
-	List<BoardListVO> searchBoardlist(String condition, String keyword);
-	
-	int updateBoardList(BoardListVO boardlist);
-	
-	int removeBoardList(int value);
-	
-	List<BoardListVO> getBoardNameList();
-}
 
+	int addPost(BoardListVO post_num) throws Exception;
+
+	BoardListVO getPost(int post_id);
+
+	List<BoardListVO> getPostList();
+
+	List<BoardListVO> searchBoardlist(String condition, String keyword);
+
+	int updateBoardList(BoardListVO boardlist);
+
+	int removeBoardList(String value);
+
+}
