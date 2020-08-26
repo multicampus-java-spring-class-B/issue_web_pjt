65160dd3b46c4e6013eaedbd3316781124a724ea
jvvv94 <67827336+jvvv94@users.noreply.github.com>
2020-08-26 20:18:51 +0900

    Update BoardListDAO.java

--- JAVA/BoardListDAO.java
+++ JAVA/BoardListDAO.java
@@ -6,16 +6,14 @@ import boardlist.vo.BoardListVO;
 
 public interface BoardListDAO {
 	int addPost(BoardListVO post_num) throws Exception;
-	
-	BoardListVO getPost(String title);
-	
+
+	BoardListVO getPost(int post_id);
+
 	List<BoardListVO> getPostList();
-	
+
 	List<BoardListVO> searchBoardlist(String condition, String keyword);
-	
+
 	int updateBoardList(BoardListVO boardlist);
-	
-	int removeBoardList(int value);
-	
-	List<BoardListVO> getBoardNameList();
-} 
+
+	int removeBoardList(String value);
+}
