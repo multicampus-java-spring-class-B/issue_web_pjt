330c877c5d725b4759d21a3389af842db45cbd48
jvvv94 <67827336+jvvv94@users.noreply.github.com>
2020-08-26 20:20:00 +0900

    Update BoardListServiceImpl.java

--- JAVA/BoardListServiceImpl.java
+++ JAVA/BoardListServiceImpl.java
@@ -3,15 +3,28 @@ package boardlist.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import boardlist.dao.BoardListDAO;
 import boardlist.vo.BoardListVO;
 
 @Component("boardlist")
+//@Service
 public class BoardListServiceImpl implements BoardListService {
 	@Autowired
+	@Qualifier("spring")
 	BoardListDAO dao;
+
+	public BoardListServiceImpl() {
+	}
+
+	public BoardListServiceImpl(BoardListDAO dao) {
+
+		this.dao = dao;
+	}
 
 	@Override
 	public int addPost(BoardListVO post_num) throws Exception {
@@ -20,9 +33,9 @@ public class BoardListServiceImpl implements BoardListService {
 	}
 
 	@Override
-	public BoardListVO getPost(String title) {
+	public BoardListVO getPost(int post_id) {
 		// TODO Auto-generated method stub
-		return dao.getPost(title);
+		return dao.getPost(post_id);
 	}
 
 	@Override
@@ -44,14 +57,9 @@ public class BoardListServiceImpl implements BoardListService {
 	}
 
 	@Override
-	public int removeBoardList(int value) {
+	public int removeBoardList(String value) {
 		// TODO Auto-generated method stub
 		return dao.removeBoardList(value);
 	}
 
-	@Override
-	public List<BoardListVO> getBoardNameList() {
-		// TODO Auto-generated method stub
-		return dao.getBoardNameList();
-	}
 }
