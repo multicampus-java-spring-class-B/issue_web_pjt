def28e6ee021cd6dbb53ec69bdbbfe07bf1bc0a5
ganggm <51116431+ganggm@users.noreply.github.com>
2020-08-27 10:33:04 +0900

    Update BoardListDAO.java

--- JAVA/BoardListDAO.java
+++ JAVA/BoardListDAO.java
@@ -6,16 +6,14 @@ import boardlist.vo.BoardListVO;
 
 public interface BoardListDAO {
 	int addPost(BoardListVO post_num) throws Exception;
-
-	BoardListVO getPost(int post_id);
-
-	List<BoardListVO> getPostList();
-
-	List<BoardListVO> searchBoardlist(String condition, String keyword);
-
-	int updateBoardList(BoardListVO boardlist);
-
-	int removeBoardList(String value);
 	
-	List<BoardListVO> getBoardNameList();
-}
+	BoardListVO getPost(int post_id);
+	
+	List<BoardListVO> getPostList();
+	
+	List<BoardListVO> searchBoardlist(String condition, String keyword);
+	
+	int updateBoardList(BoardListVO boardlist);
+	
+	int removeBoardList(int post_id);
+} 
